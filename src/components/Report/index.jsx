b87fe9edb07aacb0919beb6781ec4ebbb3cc3014
Marat Gaipov <gaimarat@amazon.com>
2019-10-23 17:29:32 -0700

    url report

--- src/components/Report/index.jsx
+++ src/components/Report/index.jsx
@@ -10,200 +10,234 @@ import './style.css';
 import {connect} from "react-redux";
 import {actions} from "../../redux/reducer";
 
+const reportApiUrl = 'https://fixinity-api-staging.herokuapp.com/api/v1/report?shortUrlCode=';
+const reportSampleData = {
+  info: {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2018,
+    odometer: 'odometer',
+    vin: '123123',
+    date: 1312313123
+  },
+  diagnostic: {
+    reportedIssue: 'reportedIssue',
+    rootCause: 'rootCause',
+    recommendation: 'recommendation',
+    pricePartsDescription: 'new rotors for Toyota Camry',
+    pricePartsAmountFrom: 200,
+    pricePartsAmountTo: 300,
+    priceLaborDescription: 'Labor and Installation',
+    priceLaborAmountFrom: 100,
+    priceLaborAmountTo: 150,
+    //    prices: [
+    //      {price:300, description:'new rotors for Toyota Camry'},
+    //      {price:100, description:'Labor and Installation'}
+    //    ],
+    photos: [
+      {
+        url: 'url',
+        name: 'tire'
+      }
+    ],
+    quotes: [
+      {
+        id: 1,
+        name: 'Mechanic A',
+        address: '81 Pioneer Way, Mountain View, CA 94040',
+        price: 300,
+        availability: 'Today(October 30) at 1pm',
+        photo: 'url'
+      },
+      {
+        id: 2,
+        name: 'Mechanic B',
+        address: '345 San Marino, North Hollywood, CA 94040',
+        price: 300,
+        availability: 'Today(October 30) at 5pm',
+        photo: 'url'
+      },
+      {
+        id: 3,
+        name: 'Mechanic C',
+        address: '123 Sey street, Los Angeles, CA 94040',
+        price: 300,
+        availability: 'Today(October 30) at 3pm',
+        photo: 'url'
+      }
+    ]
+  },
+  inspection: {
+    // dash
+    warningLightValue: '...',
+    warningLightStatus: 1,
+    // fluids
+    engineOilValue: '...',
+    engineOilStatus: 1,
+    brakeFluidValue: '...',
+    brakeFluidStatus: 1,
+    coolantValue: '...',
+    coolantStatus: 1,
+    steeringFluidValue: '...',
+    steeringFluidStatus: 1,
+    washerFluidValue: '...',
+    washerFluidStatus: 1,
+    // tires - pressure
+    pressureDFValue: '...',
+    pressureDFStatus: 1,
+    pressurePFValue: '...',
+    pressurePFStatus: 1,
+    pressureDRValue: '...',
+    pressureDRStatus: 1,
+    pressurePRValue: '...',
+    pressurePRStatus: 1,
+    // tires - treading
+    treadingDFValue: '...',
+    treadingDFStatus: 1,
+    treadingPFValue: '...',
+    treadingPFStatus: 1,
+    treadingDRValue: '...',
+    treadingDRStatus: 1,
+    treadingPRValue: '...',
+    treadingPRStatus: 1,
+    // braking system - rotor
+    rotorDFValue: '...',
+    rotorDFStatus: 1,
+    rotorPFValue: '...',
+    rotorPFStatus: 1,
+    rotorDRValue: '...',
+    rotorDRStatus: 1,
+    rotorPRValue: '...',
+    rotorPRStatus: 1,
+    // braking system - pads
+    padsDFValue: '...',
+
+    padsDFStatus: 1,
+    padsPFValue: '...',
+
+    padsPFStatus: 1,
+    padsDRValue: '...',
+
+    padsDRStatus: 1,
+    padsPRValue: '...',
+
+    padsPRStatus: 1,
+    // engine
+    coolingValue: '...',
+
+    coolingStatus: 1,
+    beltsValue: '...',
+
+    beltsStatus: 1,
+    oilLeaksValue: '...',
+
+    oilLeaksStatus: 1,
+    // engine mounts
+    mountsLeftValue: '...',
+
+    mountsLeftStatus: 1,
+    mountsRightValue: '...',
+
+    mountsRightStatus: 1,
+    mountsFrontValue: '...',
+
+    mountsFrontStatus: 1,
+    mountsRearValue: '...',
+
+    mountsRearStatus: 1,
+    mountsTorqueRodValue: '...',
+
+    mountsTorqueRodStatus: 1,
+    // battery
+    corrosionValue: '...',
+
+    corrosionStatus: 1,
+    caseLeakingValue: '...',
+
+    caseLeakingStatus: 1,
+    // seat belt
+    seatBeltValue: '...',
+    seatBeltStatus: 1
+  },
+  history: [
+    {
+      "event": "client requested a job",
+      "time": "...",
+    },
+    {
+      "event": "car picked up",
+      "time": "...",
+    },
+    {
+      "event": "inspection complete",
+      "time": "...",
+    },
+    {
+      "event": "delivered to mechanic",
+      "time": "...",
+    },
+    {
+      "event": "repair complete",
+      "time": "...",
+    },
+    {
+      "event": "quality control passed",
+      "time": "...",
+    },
+    {
+      "event": "delivered to a client",
+      "time": "...",
+    }
+  ],
+  advisor: {
+    name: 'Bakyt',
+    lastName: 'Bugubaev',
+    bio: 'blablabal'
+  }
+};
+
 class Report extends Component {
     constructor(props){
         super(props)
         this.state = {
-          report: {
-            info: {
-              make: 'Toyota',
-              model: 'Camry',
-              year: 2018,
-              odometer: 'odometer',
-              vin: '123123',
-              date: 1312313123
-            },
-            diagnostic: {
-              reportedIssue: 'reportedIssue',
-              rootCause: 'rootCause',
-              recommendation: 'recommendation',
-              pricePartsDescription: 'new rotors for Toyota Camry',
-              pricePartsAmountFrom: 200,
-              pricePartsAmountTo: 300,
-              priceLaborDescription: 'Labor and Installation',
-              priceLaborAmountFrom: 100,
-              priceLaborAmountTo: 150,
-              //    prices: [
-              //      {price:300, description:'new rotors for Toyota Camry'},
-              //      {price:100, description:'Labor and Installation'}
-              //    ],
-              photos: [
-                {
-                  url: 'url',
-                  name: 'tire'
-                }
-              ],
-              quotes: [
-                {
-                  id: 1,
-                  name: 'Mechanic A',
-                  address: '81 Pioneer Way, Mountain View, CA 94040',
-                  price: 300,
-                  availability: 'Today(October 30) at 1pm',
-                  photo: 'url'
-                },
-                {
-                  id: 2,
-                  name: 'Mechanic B',
-                  address: '345 San Marino, North Hollywood, CA 94040',
-                  price: 300,
-                  availability: 'Today(October 30) at 5pm',
-                  photo: 'url'
-                },
-                {
-                  id: 3,
-                  name: 'Mechanic C',
-                  address: '123 Sey street, Los Angeles, CA 94040',
-                  price: 300,
-                  availability: 'Today(October 30) at 3pm',
-                  photo: 'url'
-                }
-              ]
-            },
-            inspection: {
-              // dash
-              warningLightValue: '...',
-              warningLightStatus: 1,
-              // fluids
-              engineOilValue: '...',
-              engineOilStatus: 1,
-              brakeFluidValue: '...',
-              brakeFluidStatus: 1,
-              coolantValue: '...',
-              coolantStatus: 1,
-              steeringFluidValue: '...',
-              steeringFluidStatus: 1,
-              washerFluidValue: '...',
-              washerFluidStatus: 1,
-              // tires - pressure
-              pressureDFValue: '...',
-              pressureDFStatus: 1,
-              pressurePFValue: '...',
-              pressurePFStatus: 1,
-              pressureDRValue: '...',
-              pressureDRStatus: 1,
-              pressurePRValue: '...',
-              pressurePRStatus: 1,
-              // tires - treading
-              treadingDFValue: '...',
-              treadingDFStatus: 1,
-              treadingPFValue: '...',
-              treadingPFStatus: 1,
-              treadingDRValue: '...',
-              treadingDRStatus: 1,
-              treadingPRValue: '...',
-              treadingPRStatus: 1,
-              // braking system - rotor
-              rotorDFValue: '...',
-              rotorDFStatus: 1,
-              rotorPFValue: '...',
-              rotorPFStatus: 1,
-              rotorDRValue: '...',
-              rotorDRStatus: 1,
-              rotorPRValue: '...',
-              rotorPRStatus: 1,
-              // braking system - pads
-              padsDFValue: '...',
-
-              padsDFStatus: 1,
-              padsPFValue: '...',
-
-              padsPFStatus: 1,
-              padsDRValue: '...',
-
-              padsDRStatus: 1,
-              padsPRValue: '...',
-
-              padsPRStatus: 1,
-              // engine
-              coolingValue: '...',
-
-              coolingStatus: 1,
-              beltsValue: '...',
-
-              beltsStatus: 1,
-              oilLeaksValue: '...',
-
-              oilLeaksStatus: 1,
-              // engine mounts
-              mountsLeftValue: '...',
-
-              mountsLeftStatus: 1,
-              mountsRightValue: '...',
-
-              mountsRightStatus: 1,
-              mountsFrontValue: '...',
-
-              mountsFrontStatus: 1,
-              mountsRearValue: '...',
-
-              mountsRearStatus: 1,
-              mountsTorqueRodValue: '...',
-
-              mountsTorqueRodStatus: 1,
-              // battery
-              corrosionValue: '...',
-
-              corrosionStatus: 1,
-              caseLeakingValue: '...',
-
-              caseLeakingStatus: 1,
-              // seat belt
-              seatBeltValue: '...',
-              seatBeltStatus: 1
-            },
-            history: [
-              {
-                "event": "client requested a job",
-                "time": "...",
-              },
-              {
-                "event": "car picked up",
-                "time": "...",
-              },
-              {
-                "event": "inspection complete",
-                "time": "...",
-              },
-              {
-                "event": "delivered to mechanic",
-                "time": "...",
-              },
-              {
-                "event": "repair complete",
-                "time": "...",
-              },
-              {
-                "event": "quality control passed",
-                "time": "...",
-              },
-              {
-                "event": "delivered to a client",
-                "time": "...",
-              }
-            ],
-            advisor: {
-              name: 'Bakyt',
-              lastName: 'Bugubaev',
-              bio: 'blablabal'
-            }
-          }
+          loaded: false,
+          report:{}
         }
+    }
+
+    componentDidMount() {
+      const {reportId} = this.props.match.params;
+      const reportUrl = `${reportApiUrl}${reportId}`;
+      console.log('reportUrl',reportUrl)
+      fetch(reportUrl,{
+        method: 'get',
+        //mode: 'no-cors',
+      })
+        .then(response=>{
+
+          return response.text()
+          return response.json()
+
+
+          console.log(JSON.parse(response.body))
+          return response.text().then(function(text) {
+            debugger
+            return text ? JSON.parse(text) : {}
+          })
+        })
+        .then(report=>{
+          debugger
+          this.setState({report,loaded:true})
+        })
+        .catch(err=>{
+          debugger
+          this.setState({loaded:true,report:reportSampleData})
+        })
     }
 
   render() {
       const {reportId, reportPage} = this.props.match.params;
-      const { report: {info, diagnostic, inspection, history, advisor}} = this.state;
+      const { report: {info, diagnostic, inspection, history, advisor, loaded}} = this.state;
       let content = <ReportDiagnostic data={diagnostic}/>;
       if(reportPage === 'inspection'){
         content = <ReportInspection data={inspection}/>
@@ -211,6 +245,10 @@ class Report extends Component {
         content = <ReportHistory data={history} />
       }else if (reportPage === 'about') {
         content = <ReportAdvisor data={advisor} />
+      }
+
+      if(!loaded){
+        return null
       }
 
     return (
